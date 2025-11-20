7d0d051ab44d891ba5b2a5044f1fed39e6d2ef69
Kaizzendev <Kaizzendev@gmail.com>
2025-11-20 23:41:48 +0100

    feat(rapid tank, and armoured tank, changed tank materials colors)

--- Assets/Code/Scripts/EnemyTank.cs
+++ Assets/Code/Scripts/EnemyTank.cs
@@ -43,6 +43,7 @@ public class EnemyTank : MonoBehaviour
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = moveSpeed;
     }
 
     void Start()
