d69118abfbb403981a18b0862c91185be4291704
Kaizzendev <Kaizzendev@gmail.com>
2026-08-12 18:30:18 +0200

    feat: move procedural gen

--- Assets/Scripts/GeneratorSceneManager.cs
+++ Assets/Scripts/GeneratorSceneManager.cs
@@ -1,4 +1,5 @@
 using System;
+using ProceduralGeneration;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using ProceduralGeneration;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
